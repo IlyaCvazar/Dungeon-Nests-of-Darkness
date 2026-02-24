2b353087f259e3666a6f8343aac63fdbe44325a3
IlyaCvazar <ilaokolelov726@gmail.com>
2026-02-24 16:56:43 +0700

    Переработал меню

--- assets/scripts/WelcomeScript.cs
+++ assets/scripts/WelcomeScript.cs
@@ -2,23 +2,36 @@ using Godot;
 using System;
 
 
-public partial class WelcomeScript : Label
+public partial class WelcomeScript : RichTextLabel
 {
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		VisibleCharacters = 0;
 	}
 	private void _on_mouse_entered()
 	{
 		VisibleCharacters = 0;
+		GD.Print("Time out " + GetTotalCharacterCount());
+		Timer timer = GetNode<Timer>($"../Timer");
+		timer.Start();
 	}
 	
 	private void _on_timer_timeout()
 	{
-	VisibleCharacters += 1;
-		
-	} 
+	if (VisibleCharacters < GetTotalCharacterCount())
+	{
+		VisibleCharacters += 1;
+		GD.Print("Line_With: " + GetTotalCharacterCount());
+		} 
+		else
+		{
+			Timer timer = GetNode<Timer>($"../Timer");
+			timer.Stop();
+			GD.Print("He");
+		}
+	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
